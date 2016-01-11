922fc5b8ccf4b291a10512b992dbd9f856540fd0
pro2s <protasov@vc-64151.elmotor.org>
2016-01-11 15:31:48 +0300

    full ngresource support on menu

--- Food.Web/app/controllers/menu.js
+++ Food.Web/app/controllers/menu.js
@@ -1,16 +1,25 @@
 ﻿'use strict';
 
 angular.module('FoodApp.Menu', []).controller('MenuCtrl',['$scope','Menu', function ($scope, Menu) {
-    $scope.title = "loading menu ...";
+    $scope.title = "Loading menu ...";
     $scope.options = [];
     $scope.working = false;
 
-    $scope.Menu = function (components) {
-        $scope.working = true;
-        $scope.tomorrow = false;
-        $scope.components = typeof components !== 'undefined' ? components : true;
+    $scope.Menu = function (components = true) {
         $scope.date = new Date();  
-       
-		$scope.weekmenu = Menu.query();
+		$scope.tomorrow = false;
+        $scope.working = true;
+		$scope.components = components;
+		
+		var success = function(menu){
+			$scope.working = false;
+			$scope.title = "Week Menu";
+			$scope.weekmenu = menu;
+		};
+		var failure = function(data){
+			$scope.title = "Oops... something went wrong";
+			$scope.working = false;
+		};
+		Menu.query(success, failure);
     };
 }]);
