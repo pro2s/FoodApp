e8d0f64cef2db7c63a1f6389050589f1af3557e4
unknown <protasov@vc-64151.elmotor.org>
2016-01-11 14:58:15 +0300

    add ngResource support

--- Food.Web/app/controllers/menu.js
+++ Food.Web/app/controllers/menu.js
@@ -1,25 +1,16 @@
 ﻿'use strict';
 
-angular.module('FoodApp.Menu', []).controller('MenuCtrl', function ($scope, $http, menuService) {
+angular.module('FoodApp.Menu', []).controller('MenuCtrl',['$scope','Menu', function ($scope, Menu) {
     $scope.title = "loading menu ...";
     $scope.options = [];
     $scope.working = false;
 
     $scope.Menu = function (components) {
         $scope.working = true;
-        $scope.options = [];
         $scope.tomorrow = false;
         $scope.components = typeof components !== 'undefined' ? components : true;
-        $scope.date = new Date();
-        $scope.weekmenu = menuService.getMenu();
-        $scope.working = menuService.status;
-        $http.get("http://localhost:52959/api/values").success(function (data, status, headers, config) {
-            $scope.choice = data.choiceSet;
-            $scope.title = "Menus";
-            //$scope.working = false;
-        }).error(function (data, status, headers, config) {
-            $scope.title = "Oops... something went wrong";
-            //$scope.working = false;
-        });
+        $scope.date = new Date();  
+       
+		$scope.weekmenu = Menu.query();
     };
-})
+}]);
--- Food.Web/app/services/menu.js
+++ Food.Web/app/services/menu.js
@@ -1,24 +1,8 @@
 ﻿'use strict';
-angular.module('FoodApp.MenuService', [])
-.factory('menuService', function ($rootScope, $http) {
-    var menuService = {};
-    
-    menuService.data = {};
-    menuService.status = false;
-	
-    //Gets the list of nuclear weapons
-    menuService.getMenu = function () {
-        menuService.status = false;
-        $http.get($rootScope.api + 'api/menu')
-            .success(function (data) {
-                menuService.data = data;
-                menuService.status = true;
-            })
-            .error(function (data, status, headers, config) {
-                menuService.status = true;
-            });
-        return menuService.data;
-    };
-
-    return menuService;
-});
+angular.module('FoodApp.MenuService', ['ngResource'])
+.factory('Menu', ['$rootScope','$resource',
+  function($rootScope, $resource){
+    return $resource($rootScope.api + 'api/menu/:menuId', {}, {
+      query: {method:'GET', params:{menuId:''}, isArray:true}
+    });
+  }])
