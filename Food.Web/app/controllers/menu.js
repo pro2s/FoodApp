b924f867570d53919409d42b0501b7a98d354645
pro2s <protasov@vc-64151.elmotor.org>
2016-01-14 14:06:41 +0300

    working form for menu

--- Food.Web/app/controllers/menu.js
+++ Food.Web/app/controllers/menu.js
@@ -1,19 +1,65 @@
 ﻿'use strict';
 
-angular.module('FoodApp.Menu', []).controller('MenuCtrl',['$scope','Menu', function ($scope, Menu) {
+angular.module('FoodApp.Menu', [])
+.controller('MenuCtrl',['$scope','Menu', function ($scope, Menu) {
     $scope.title = "Loading menu ...";
     $scope.options = [];
     $scope.working = false;
-
+	$scope.menu = {};
+	$scope.menu.items = [];
+	$scope.editeditem = {};
+	$scope.menuitem = {};
+	
+	$scope.additem = function(item) {
+		$scope.menu.items.push(angular.copy(item));
+	};
+	
+	$scope.edititem = function(item) {
+		$scope.editeditem = item;
+	};
+	
+	$scope.canceledit = function() {
+		$scope.editeditem = {};
+	};
+	
+	$scope.saveitem = function() {
+		//  edit item in base
+		$scope.editeditem = {};
+	};
+	
+	$scope.deleteitem = function(item) {
+		//  remove item from base
+		for (var i = 0; $scope.menu.items.length; i++) {
+			if ($scope.menu.items[i] === item) {
+				$scope.menu.items.splice(i, 1);
+				break; 
+			}
+		}
+	};
+	
+	$scope.edited = function(item) {
+		return $scope.editeditem === item;
+	};
+	
+	$scope.add = function() {
+		$scope.menu = {};
+		$scope.menu.items = [];
+		$scope.menuitem = {}
+		$scope.edit = true;
+	};
+	
+	$scope.editMenu = function(menu) {
+		$scope.add();
+		$scope.menu = menu;
+	};
+	
     $scope.Menu = function (components = true) {
         $scope.date = new Date();  
 		$scope.tomorrow = false;
         $scope.working = true;
 		$scope.edit = false;
 		$scope.components = components;
-		$scope.add = function() {
-			$scope.edit = true;
-		};
+		
 		var success = function(menu){
 			$scope.working = false;
 			$scope.title = "Week Menu";
