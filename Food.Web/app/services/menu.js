fc4d44d78ff85fdbe95639f85aad53d9f5865f0e
pro2s <protasov.s.s@gmail.com>
2016-01-17 21:42:55 +0300

    fix error

--- Food.Web/app/services/menu.js
+++ Food.Web/app/services/menu.js
@@ -3,7 +3,7 @@ angular.module('FoodApp.MenuService', ['ngResource'])
 .factory('Menu', ['$rootScope','$resource',
   function($rootScope, $resource){
     return $resource($rootScope.api + 'api/menu/:id', {}, {
-      query: {method:'GET', params:{id:''}, isArray:true}
+      query: {method:'GET', params:{id:''}, isArray:true},
 	  update: {method:'PUT', params:{id:''}, isArray:true}
     });
   }])
