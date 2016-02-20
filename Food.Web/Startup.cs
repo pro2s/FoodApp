19c2c111f442b926950bb4b14c6001201d261026
pro2s <protasov.s.s@gmail.com>
2016-02-20 13:11:04 +0300

    Cleanup Web UI

--- Food.Web/Startup.cs
+++ Food.Web/Startup.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(Food.Web.Startup))]
+
 namespace Food.Web
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+           
         }
     }
 }
