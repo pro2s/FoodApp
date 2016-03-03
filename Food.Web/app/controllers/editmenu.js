3d0e3c71e8e864381255f1f94d3703473b262143
pro2s <work@pro2s.ru>
2016-03-03 10:58:48 +0300

    Move menu edit form to directive

--- Food.Web/app/controllers/editmenu.js
+++ Food.Web/app/controllers/editmenu.js
@@ -10,67 +10,13 @@
         var form = this;
         form.title = "New Menu";
         form.menu = {};
-        form.menu.items = [];
-        form.editeditem = {};
-        form.backupitem = {};
-        form.menuitem = {};
-        form.isedit = false;
+        form.backupmenu = {};
         form.parse = {};
 
-        form.additem = addItem;
-        form.edititem = editItem;
-        form.cancelitem = cancelItem;
-        form.saveitem = saveItem;
-        form.deleteItem = deleteItem;
-        form.edited = edited;
-        
         form.add = add;
         form.edit = edit;
         form.cancel = cancel;
         form.save = save;
-        
-        
-        
-        
-
-        function addItem(item) {
-            item.id = 0;
-            form.menu.items.push(angular.copy(item));
-        };
-        
-        function editItem(item) {
-            form.backupitem = angular.copy(item);
-            form.editeditem =  item;
-        };
-        
-        function cancelItem() {
-            angular.copy(form.backupitem,form.editeditem);
-            form.editeditem = {};
-        };
-        
-        function saveItem () {
-            var item = new MenuItem(form.editeditem);
-            if (item.id != 0) {
-                item.$update({id:item.id});
-            }
-            form.editeditem = {};
-        };
-        
-        function deleteItem(item) {
-            for (var i = 0; form.menu.items.length; i++) {
-                if (form.menu.items[i] === item) {
-                    if (item.id > 0){
-                        MenuItem.delete({id:item.id});
-                    }
-                    form.menu.items.splice(i, 1);
-                    break; 
-                }
-            }
-        };
-        
-        function edited(item) {
-            return form.editeditem === item;
-        };
         
         function add() {
             form.title = "New menu";
@@ -84,10 +30,12 @@
         function edit(menu) {
             form.add();
             form.title = "Edit menu";
+            form.backupmenu = angular.copy(menu);
             form.menu = menu;
         };
         
         function cancel() {
+            angular.copy(form.backupmenu,form.menu);
             form.isedit = false;
         }
         
---
+++ Food.Web/app/directives/menuform.js
@@ -0,0 +1,76 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('app')
+        .directive('menuForm', menuForm);
+
+    
+    function menuForm() {
+        
+        var directive = {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                menu: '=',
+                title: '=',
+                save: '&',
+                cancel: '&',
+            },
+            templateUrl: 'app/views/menuform.html',
+            link: function (scope, element, attrs) {
+                scope.editeditem = {};
+                scope.backupitem = {};
+                scope.menuitem = {};
+                scope.addItem = addItem;
+                scope.editItem = editItem;
+                scope.cancelItem = cancelItem;
+                scope.saveItem = saveItem;
+                scope.deleteItem = deleteItem;
+                scope.edited = edited;
+
+
+                activate();
+
+                function activate() {
+                }
+
+
+                function addItem(item) {
+                    item.id = 0;
+                    scope.menu.items.push(angular.copy(item));
+                };
+                
+                function editItem(item) {
+                    scope.backupitem = angular.copy(item);
+                    scope.editeditem =  item;
+                };
+                
+                function cancelItem() {
+                    angular.copy(scope.backupitem,scope.editeditem);
+                    scope.editeditem = {};
+                };
+                
+                function saveItem () {
+                    scope.editeditem = {};
+                };
+                
+                function deleteItem(item) {
+                    for (var i = 0; scope.menu.items.length; i++) {
+                        if (scope.menu.items[i] === item) {
+                            scope.menu.items.splice(i, 1);
+                            break; 
+                        }
+                    }
+                };
+                
+                function edited(item) {
+                    return scope.editeditem === item;
+                };
+                
+            }
+        };
+        return directive;
+
+    };
+
+})();
