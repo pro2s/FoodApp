72296aa532726d0453ec7ded9671a28f06e01cef
pro2s <work@pro2s.ru>
2016-03-03 10:59:37 +0300

    Prepare to get list of food services

--- Food.Web/app/directives/parseform.js
+++ Food.Web/app/directives/parseform.js
@@ -16,15 +16,37 @@
             templateUrl: 'app/views/parse.html',
             link: function (scope, element, attrs) {
                 scope.id = 'ParseMenu';
+                scope.parseForm = {}
+                scope.sources = {},
+                scope.source = {},
                 scope.internalControl = scope.control || {};
                 scope.internalControl.show = show;
                 scope.internalControl.hide = hide;
+                scope.setSource = setSource;
                 scope.doParse = doParse;
 
 
                 activate();
 
                 function activate() {
+                    scope.sources = [
+                    {
+                        id:0,
+                        icon:'http://chudo-pechka.by/assets/templates/Chudopechka/images/logo.png',
+                        name:'Cudo Pechka HTML'
+                    },
+                    {
+                        id:1,
+                        icon:'http://chudo-pechka.by/assets/templates/Chudopechka/images/logo.png',
+                        name:'Cudo Pechka DOC'
+                    },
+                    {
+                        id:2,
+                        icon:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald's_Golden_Arches.svg/200px-McDonald's_Golden_Arches.svg.png",
+                        name:'McDonalds'
+                    },
+                    ]
+                    scope.source = scope.sources[0];
                 }
 
 
@@ -35,7 +57,9 @@
                 function hide() {
                     $('#' + scope.id).modal('hide');
                 }
-
+                function setSource(source) {
+                    scope.source = source;
+                }
                 function doParse() {
 
                 }
